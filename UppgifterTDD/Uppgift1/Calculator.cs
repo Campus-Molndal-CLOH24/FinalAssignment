19ac89e1957e55346c503acd55ed23713cccf013
maria schillstrom <mariacschillstrom@gmail.com>
2025-01-12 13:12:24 +0100

    Divide, Multiply and Subtract testes done

--- UppgifterTDD/Uppgift1/Calculator.cs
+++ UppgifterTDD/Uppgift1/Calculator.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FinalAssignment.UppgifterTDD.Uppgift1
 {
     public class Calculator
+    //ADD 
     {
         public double AddFromInput(string inputA, string inputB)
         {
@@ -25,7 +27,53 @@ namespace FinalAssignment.UppgifterTDD.Uppgift1
             else
             {
                 throw new ArgumentException("Ogiltig inmatning! Ange giltiga siffror.");  // Kasta undantag vid felaktig inmatning tex abc 
+            }
         }
+        // DIVIDE
+       
+        public double DivideFromInput(string inputA, string inputB)
+        {
+            if (double.TryParse(inputA, out double a) && double.TryParse(inputB, out double b))
+            {
+                if (b == 0)
+                {
+                    throw new ArgumentException("Kan inte dela med noll.");  // Kasta undantag om b == 0
+                }
+                return a / b;  // Utför division om båda inmatningarna är giltiga tal
+            }
+            else
+            {
+                throw new ArgumentException("Ogiltig inmatning! Ange giltiga siffror.");  // Kasta undantag för t.ex. "abc"
+            }
+        }
+        // MULTIPLY
+        public double MultiplyFromInput(string inputA, string inputB)
+        {
+            if (double.TryParse(inputA, out double a) && double.TryParse(inputB, out double b))
+            {
+                return a * b;  // Utför multiplikation om båda inmatningarna är giltiga tal
+            }
+            else
+            {
+                throw new ArgumentException("Ogiltig inmatning! Ange giltiga siffror.");  // Kasta undantag för t.ex. "abc"
+            }
     }
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
