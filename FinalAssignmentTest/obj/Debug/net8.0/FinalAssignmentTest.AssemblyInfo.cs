aa5c0f562d54bf715246a9bb179dce6513a6a7c5
maria schillstrom <mariacschillstrom@gmail.com>
2025-01-12 21:05:41 +0100

    ingen aning

--- FinalAssignmentTest/obj/Debug/net8.0/FinalAssignmentTest.AssemblyInfo.cs
+++ FinalAssignmentTest/obj/Debug/net8.0/FinalAssignmentTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalAssignmentTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348029507af418e868e12064f053b660ddcee5cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalAssignmentTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalAssignmentTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
